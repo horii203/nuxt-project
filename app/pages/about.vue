fd6197047688fa14e7aaf2d57163df5d3d68e117
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 12:16:30 +0900

    ボタンコンポーネント

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>about</h1>
-
+    <MyButton />
+    <br />
     <NuxtLink to="/"> トップページへ戻る </NuxtLink>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>index</h1>
+    <MyButton />
+    <br />
     <NuxtLink to="/about"> Aboutページへ </NuxtLink>
   </div>
 </template>
