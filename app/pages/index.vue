8870bb503106ab1917087d6bcc3a782ab550a474
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 18:18:51 +0900

    TODOリスト

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,34 +1,58 @@
 <script setup>
 import { ref } from "vue";
+const task = ref("");
+const tasks = ref([
+  {
+    id: 1,
+    title: "Nuxtを勉強する",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Vueを復習する",
+    completed: true,
+  },
+]);
 
-const fruit = ref("");
+const addTask = () => {
+  if (task.value === "") {
+    return;
+  }
 
-const fruits = ref(["りんご", "みかん"]);
+  tasks.value.push({
+    id: Date.now(),
+    title: task.value,
+    completed: false,
+  });
 
-const addFruit = () => {
-  fruits.value.push(fruit.value);
-  fruit.value = "";
+  task.value = "";
 };
 
-const deleteFruit = (index) => {
-  // splice は配列から要素を削除する
-  fruits.value.splice(index, 1);
+const deteleTask = (index) => {
+  tasks.value.splice(index, 1);
 };
 </script>
 
 <template>
   <div>
-    <h1>フルーツ一覧</h1>
+    <h1>Todo List</h1>
 
-    <input v-model="fruit" type="text" placeholder="フルーツを入力" />
-
-    <button @click="addFruit">追加</button>
+    <input type="text" v-model="task" />
+    <button @click="addTask">追加</button>
 
     <ul>
-      <li v-for="(item, index) in fruits" :key="item">
-        {{ item }}
-        <button @click="deleteFruit(index)">削除</button>
+      <li v-for="(item, index) in tasks" :key="item.id">
+        <label>
+          <input type="checkbox" v-model="item.completed" />
+          <span :class="{ completed: item.completed }">{{ item.title }}</span>
+        </label>
+        <button @click="deteleTask">削除</button>
       </li>
     </ul>
   </div>
 </template>
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
