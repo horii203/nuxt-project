5fff75aaac6550f5909c43d7b92aecfb33a08d01
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 12:12:44 +0900

    リンク追加

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <h1>about</h1>
+
+    <NuxtLink to="/"> トップページへ戻る </NuxtLink>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>index</h1>
+    <NuxtLink to="/about"> Aboutページへ </NuxtLink>
   </div>
 </template>
