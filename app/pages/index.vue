9d4ad4a7b8aa403fc7f7348b04053be02eedec4c
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 13:38:04 +0900

    リストに追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,28 @@
 <script setup>
-const fruits = ["りんご", "みかん", "バナナ"];
+import { ref } from "vue";
+
+const fruit = ref("");
+
+const fruits = ref(["りんご", "みかん"]);
+
+const addFruit = () => {
+  fruits.value.push(fruit.value);
+  fruit.value = "";
+};
 </script>
 
 <template>
   <div>
+    <h1>フルーツ一覧</h1>
+
+    <input v-model="fruit" type="text" placeholder="フルーツを入力" />
+
+    <button @click="addFruit">追加</button>
+
     <ul>
-      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+      <li v-for="item in fruits" :key="item">
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
