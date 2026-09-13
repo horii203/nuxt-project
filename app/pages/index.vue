8bf77479288ee90774d68df2e3c9a1682c57dc71
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 20:52:26 +0900

    useFetch

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello");
+</script>
+
 <template>
-  <h1>トップページ</h1>
-  <p>ここがページ固有の内容です。</p>
+  <div>
+    <h1>トップページ</h1>
+
+    <p>{{ data?.message }}</p>
+    <p>{{ data?.name }}</p>
+  </div>
 </template>
