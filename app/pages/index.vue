b3b2f52cd312e2a03477f8fb2e6fdc3aa5f9b5d7
horii203 <89522294+horii203@users.noreply.github.com>
2026-09-13 13:35:27 +0900

    v-for

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,11 @@
 <script setup>
-import { ref } from "vue";
-const count = ref(0);
-const increment = () => {
-  count.value++;
-};
+const fruits = ["りんご", "みかん", "バナナ"];
 </script>
 
 <template>
   <div>
-    <p>カウント：{{ count }}</p>
-    <button @click="increment">Increment</button>
-    <p v-if="count >= 5">5以上になりました！</p>
-    <p v-else>5未満です</p>
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+    </ul>
   </div>
 </template>
